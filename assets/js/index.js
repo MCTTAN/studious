dac82b4ef41ec3ee421a7a199d5889e14c9f716f
rising-jsmn <ttru17@gmail.com>
2020-04-14 15:30:31 -0400

    commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,7 +10,8 @@ firebase.auth().onAuthStateChanged(function(user) {
     if(user != null){
 
       var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+      window.location.href = "mcttan.github.io/studious/home.html";
+      // document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
 
     }
 
