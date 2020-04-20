91f390c85ffcbed97241e184a544e469aef2ccf9
rising-jsmn <ttru17@gmail.com>
2020-04-20 00:32:04 -0400

    commit

--- assets/php/index.php
+++ assets/php/index.php
@@ -4,30 +4,37 @@
     <title>PHP File Upload</title>
   </head>
   <body>
-    
+  
     <?php
       if(isset($_FILES['userfile'])){
-      
+          pre_r($_FILES);
+        
         $phpFileUploadErrors = array(
           0 => 'There is no error, the file uploaded with success',
           1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
           2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
-          3 => The uploaded file was only partially uploaded,
+          3 => 'The uploaded file was only partially uploaded',
           4 => 'No file was uploaded',
           6 => 'Missing a temporary folder',
           7 => 'Failed to write file to disk',
-          8 => 'A PHP extension stopped the file upload'
+          8 => 'A PHP extension stopped the file upload',
         );
         
         $ext_error = false;
-        $extensions = array('jpg', 'jpeg', 'gif', 'png'); // Extensions allowed for upload
+        
+        // Only accept certain files
+        $extensions = array('jpg', 'jpeg', 'gif', 'png', 'txt', 'docx', 'pdf', 'xml');
+        
+        // Get file extension name
         $file_ext = explode('.', $_FILES['userfile']['name']);
         $file_ext = end($file_ext);
+        
+        // If file isn't an image, then set error to TRUE
         if(!in_array($file_ext, $extensions)){
           $ext_error = true;
         }
         
-        // If the error of the upload is not equal to zero
+        // If the error of the upload is not equal to 0, then there are errors
         if($_FILES['userfile']['error']){
           echo $phpFileUploadErrors[$_FILES['userfile']['error']];
         }
@@ -37,7 +44,8 @@
         else {
           echo "Success. Image has been uploaded.";
         }
-        move_uploaded_files($_FILES['uderfile']['tmp_name'], 'C:/Users/marie/Documents/studious/uploads/'.$_FILES['userfile']['name']);
+        
+        move_uploaded_file($_FILES['userfile']['tmp_name'], ''.$_FILES['userfile']['tmp_name']);
       }
       function pre_r($array){
         echo '<pre>';
@@ -45,10 +53,11 @@
         echo '</pre>';
       }
     ?>
-    
+  
     <form action="" method="POST" enctype="multipart/form-data">
       <input type="file" name="userfile">
       <input type="submit" value="Upload">
     </form>
+    
   </body>
 </html>
